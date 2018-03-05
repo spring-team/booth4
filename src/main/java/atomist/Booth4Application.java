a4b3c7d899a1526a567a76f7efc9c440d79f6bb4
Rod Johnson <johnsonroda@gmail.com>
2018-03-05 14:45:22 +0000

    Initial commit from Atomist

---
+++ src/main/java/atomist/Booth4Application.java
@@ -0,0 +1,12 @@
+package atomist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Booth4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Booth4Application.class, args);
+	}
+}
